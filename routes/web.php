20104167805fb9692f7997c70f8509bdc200cea1
truong04052003 <tranvantruong10b2@gmail.com>
2022-11-30 10:50:44 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//login 
+Route::get('/login', function () {
+    return view('login');
+});
+//vào trang register
+Route::get('/register', function () {
+    echo '<h1> trang dang ký</h1> ';
+});
+
+
+//xu ly login
+Route::post('/xu-ly-login', function (Request  $request) {
+    // dd($request->all());
+    $username = $request->username;
+    $password = $request->password;
+    echo $username . ' - ' . $password;
+    die();
+});
+//xu ly register
+Route::post('/xu-ly-register', function () {
+});
+
+
+//bài tập calculator
+
+//hiển thị form calculator
+Route::get('/calculator', function () {
+    return view('calculator');
+});
+
+//xu ly calculator
+Route::post('/xu-ly-calculator', function (Request  $request) {
+    // dd($request->all());
+    // die();
+    $product_description = $request->product_description;
+    $list_price = $request->list_price;
+    $discount_percent = $request->discount_percent;
+    // echo $product_description . ' - ' . $list_price . ' - ' . $discount_percent;
+    $discount_amount = $list_price * $discount_percent * 0.1;
+    echo $discount_amount;
+});
